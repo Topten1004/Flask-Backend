30780cd539284d3f13e30c0cd90e399c93f23499
CrazyCoding330 <97684719+TopTen1004@users.noreply.github.com>
2023-01-21 19:27:47 -0600

    initial version

---
+++ flask-vue-crud/client/src/main.js
@@ -0,0 +1,14 @@
+import BootstrapVue from 'bootstrap-vue';
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import 'bootstrap/dist/css/bootstrap.css';
+
+Vue.use(BootstrapVue);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount('#app');
